1766e957af0c48c224e0ce0b6e8053be6fff5f94
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2023-06-17 17:23:07 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,67 +1,75 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors"); // Import the cors module
-const app = express();
-const port = 8000;
+// const express = require("express");
+// const { MongoClient } = require("mongodb");
+// const mongoose = require("mongoose");
+// const serverless = require("serverless-http");
 
-// Middleware to parse JSON data
-app.use(express.json());
-app.use(cors()); // Use the cors middleware
+// const app = express();
+// const port = 8000;
+// const mongoURL =
+//   "mongodb+srv://Prayansh811:Prayansh811@cluster0.u7jcnaf.mongodb.net/webapps?retryWrites=true&w=majority";
 
-// MongoDB connection string
-const uri =
-  "mongodb+srv://Prayansh811:Prayansh811@cluster0.u7jcnaf.mongodb.net/webapps?retryWrites=true&w=majority"; // Replace with your MongoDB connection string
+//   const userSchema = new mongoose.Schema({
+//     firstName: String,
+//     lastName: String,
+//     email: String,
+//     phone: Number,
+//     age: Number,
+//     role: String,
+//   });
 
-// Create a Mongoose schema
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  phone: Number,
-  age: Number,
-  role: String,
-});
+//   // Create a Mongoose model
+//   const User = mongoose.model("User", userSchema);
 
-// Create a Mongoose model
-const User = mongoose.model("User", userSchema);
+// app.get("/items", async (req, res) => {
+//   try {
+//     const usersData = await User.find();
+//     return res.status(200).json({
+//       success: true,
+//       users: usersData,
+//     });
+//   } catch (error) {
+//     return res.status(401).json({ success: false, message: "users not found" });
+//   }
+// });
 
-// Connect to MongoDB
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => console.error("Failed to connect to MongoDB:", err));
+// app.post("/submit", async (req, res) => {
+//     const formData = req.body;
 
-// GET endpoint to retrieve paginated table data
-app.get("/items", async (req, res) => {
-  try {
-    const usersData = await User.find();
-    return res.status(200).json({
-      success: true,
-      users: usersData,
-    });
-  } catch (error) {
-    return res.status(401).json({ success: false, message: "users not found" });
-  }
-});
+//     // Insert the form data into the MongoDB collection
+//     const newUser = new User(formData);
 
-// POST endpoint to handle form submission
-app.post("/submit", async (req, res) => {
-  const formData = req.body;
+//     try {
+//       await newUser.save();
+//       res.json({ success: true, message: "Form submitted successfully!" });
+//     } catch (error) {
+//       console.error("Error storing data in MongoDB:", error);
+//       res.status(500).json({ error: "Failed to submit form" });
+//     }
+// });
 
-  // Insert the form data into the MongoDB collection
-  const newUser = new User(formData);
+// MongoClient.connect(mongoURL, (err, client) => {
+//   if (err) {
+//     console.error("Error connecting to MongoDB:", err);
+//     return;
+//   }
 
-  try {
-    await newUser.save();
-    res.json({ message: "Form submitted successfully!" });
-  } catch (error) {
-    console.error("Error storing data in MongoDB:", error);
-    res.status(500).json({ error: "Failed to submit form" });
-  }
-});
+//   const db = client.db(dbName);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+//   // Connect to MongoDB using Mongoose
+//   mongoose
+//     .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//       console.log("Connected to MongoDB");
+
+//       // Start the Express.js server inside the MongoDB connection callback
+//       const handler = serverless(app);
+//       module.exports = { handler };
+
+//       app.listen(port, () => {
+//         console.log(`Server is running on http://localhost:${port}`);
+//       });
+//     })
+//     .catch((error) => {
+//       console.error("Error connecting to MongoDB:", error);
+//     });
+// });
